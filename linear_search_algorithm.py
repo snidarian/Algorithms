035b8367d1cd93f5f8db8c75621cf9b5070121ae
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-01-17 14:54:26 -0500

    Add files via upload
    Linear search algorithm implemented in python

---
+++ linear_search_algorithm.py
@@ -0,0 +1,32 @@
+# simple linear search algorithm implemented in python
+import random
+
+num_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+             21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]
+
+random_number = random.randint(1, 38)
+guess = 1
+
+while True:
+
+    if guess == num_array[random_number - 1]:
+        print("Number found after " + str(guess) + " guesses")
+        print("The random number was " + str(random_number))
+        break
+    else:
+        print("Searching...")
+        guess += 1
+        continue
+
+
+
+
+
+
+
+
+
+
+
+
+
