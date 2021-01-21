e79fc57c247582c8be8c2e8d253746e77bfae872
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-01-21 08:27:26 -0500

    Add files via upload
    Selection sort algorithm implemented in JavaScript. The function sorts integer arrays.
    supplying the function with array arguments that contain repeated values does not break it.

---
+++ selectionSortAlgorithm.js
@@ -0,0 +1,70 @@
+
+// algorithm effectively sorts number arrays 
+// providing array arguments to the function that contain repeated values does not break the function.
+
+
+var numArray0 = [33, 67, 43, 47, 20, 9, 143, 635, 746, 456, 242, 65]
+var numArray1 = [46, 90, 32, 45, 67, 34, 132, 575, 846, 354, 245, 265]
+var numArray2 = [42, 57, 120, 45, 46, 98, 3, 24, 464, 1000, 123, 45, 63]
+var numArray3 = [3, 135, 645, 543, 74566, 375, 6, 735, 3754, 36546, 745]
+var numArray4 = [7, 64, 235, 234, 6234, 624, 432, 63, 534, 235, 6345, 64]
+var numArray5 = [1, 234, 634, 45, 64, 32, 74, 45, 7345, 344, 532, 534, 534, 234]
+
+var swap = function(array, firstIndex, secondIndex)
+{
+    var tempvar = array[firstIndex] ;
+    array[firstIndex] = array[secondIndex] ;
+    array[secondIndex] = tempvar ;
+}
+
+
+var compareValue = function(array, startIndex)
+{
+    var minValue = array[startIndex]
+    var minIndex = startIndex;
+    
+    for(j=startIndex; j < array.length; ++j)
+    {
+        // array[startIndex] compare with all other array values
+        
+
+        if (minValue > array[j])
+        {
+            minValue = array [j]
+            minIndex = j
+        }
+
+
+    }    
+    
+    if (minValue < array[startIndex])
+    {
+        swap(array, startIndex, minIndex)
+    }
+    return 0;
+}
+
+
+var selectionSort = function(array)
+{
+    for(i=0;i < array.length; i++)
+    {
+        compareValue(array, i)
+    }
+
+}
+
+
+selectionSort(numArray0)
+selectionSort(numArray1)
+selectionSort(numArray2)
+selectionSort(numArray3)
+selectionSort(numArray4)
+selectionSort(numArray5)
+
+console.log(numArray0)
+console.log(numArray1)
+console.log(numArray2)
+console.log(numArray3)
+console.log(numArray4)
+console.log(numArray5)
