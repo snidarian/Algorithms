c0d9a9904a6052c7fa38a2f741a8ae46b6776204
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-06 14:59:13 -0500

    Create recursive_factorial.pas

---
+++ recursive_factorial.pas
@@ -0,0 +1,18 @@
+// recursive factorial function
+
+program Fact_rec;
+function factorial(n: integer): integer;
+begin
+  if n>1 then
+    factorial:=n*factorial(n-1)
+  else
+    factorial:=1;
+end;
+var n, f: integer;
+begin
+  write('n = ');
+  readln(n);
+  f:=factorial(n);
+  writeln(n,'! = ',f);
+  readln;
+end.
