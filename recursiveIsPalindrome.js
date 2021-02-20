d1ee7bf901dd95eade2802d5d333aab8df25c1f1
j0rdi3 <69121571+snidarian@users.noreply.github.com>
2021-02-20 18:56:49 -0500

    remove padding

--- recursiveIsPalindrome.js
+++ recursiveIsPalindrome.js
@@ -1,22 +1,18 @@
 // recursive function set determines if string is palindrome
 
-
 // Returns the first character of the string str
 var firstCharacter = function(str) {
     return str.slice(0, 1);
 };
-
 // Returns the last character of a string str
 var lastCharacter = function(str) {
     return str.slice(-1);
 };
-
 // Returns the string that results from removing the first
 //  and last characters from str
 var middleCharacters = function(str) {
     return str.slice(1, -1);
 };
-
 var isPalindrome = function(str) {
     // base case 1
     if (str.length === 0 || str.length === 1)
@@ -48,12 +44,3 @@ checkPalindrome("rotor");
 console.log(isPalindrome("rotor"));
 console.log(isPalindrome("racecar"));
 console.log(isPalindrome("carpet"));
-
-
-
-
-
-
-
-
-
